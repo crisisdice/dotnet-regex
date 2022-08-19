6987e3297197369c2b7747696036fa9fd3e241f0
Alex Daily <alexanderdaily001@gmail.com>
2022-08-19 12:08:15 +0200

    chmod

--- comment.sh
+++ comment.sh
@@ -1,1 +1,1 @@
-for tuple in $(cat out | grep -oE '[A-Za-z]*\.cs\([0-9]*' | sort | uniq); do arr=($(echo $tuple | tr "(" " ") ); sed -i '' -e "${arr[2]} s/^/\\\\\\\\/" "${arr[1]}" ; done
+for tuple in $(cat out | grep -oE '[A-Za-z]*\.cs\([0-9]*' | sort | uniq); do arr=($(echo $tuple | tr "(" " ") ); sed -i '' -e "${arr[1]} s/^/\\\\\\\\/" "${arr[0]}" ; done
